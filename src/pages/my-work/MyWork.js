1f153591d80fc5999846ef219bc0bb04521c9624
Ristirianto Adi <ristiriantoadi@gmail.com>
2024-01-20 16:22:40 +0700

    table my work

--- src/pages/my-work/MyWork.js
+++ src/pages/my-work/MyWork.js
@@ -1,5 +1,23 @@
+import { Container, Row, Table } from "react-bootstrap";
+
 function MyWork() {
-  return <div>Karya Saya</div>;
+  return (
+    <Container>
+      <Row>
+        <Table bordered>
+          <thead>
+            <tr>
+              <th>Judul</th>
+              <th>Jenis</th>
+              <th>Media</th>
+              <th>Tanggal Terbit</th>
+              <th>Bukti Pemuatan</th>
+            </tr>
+          </thead>
+        </Table>
+      </Row>
+    </Container>
+  );
 }
 
 export default MyWork;
